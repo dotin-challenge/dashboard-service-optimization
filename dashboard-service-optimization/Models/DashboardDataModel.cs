af31cd8bc72b07dcfe94d32101475fb6d441190d
ehsan.soltani <ehsansoltani3398@gmail.com>
2025-11-01 12:22:50 +0330

    remove unnessary method

--- dashboard-service-optimization/Models/DashboardDataModel.cs
+++ dashboard-service-optimization/Models/DashboardDataModel.cs
@@ -10,25 +10,22 @@ namespace dashboard_service_optimization.Models
     {
         public byte Month { get; set; }
         public double Value { get; set; }
-        public string MonthTitle => GetTitle();
-
-        public string GetTitle()
-            => Month switch
-            {
-               1 => "January",
-               2 => "February",
-               3 => "March",
-               4 => "April",
-               5 => "May",
-               6 => "June",
-               7 => "July",
-               8 => "August",
-               9 => "September",
-               10 => "October",
-               11 => "November",
-               12 => "December",
-                _ => string.Empty
-            };
+        public string MonthTitle => Month switch
+        {
+            1 => "January",
+            2 => "February",
+            3 => "March",
+            4 => "April",
+            5 => "May",
+            6 => "June",
+            7 => "July",
+            8 => "August",
+            9 => "September",
+            10 => "October",
+            11 => "November",
+            12 => "December",
+            _ => string.Empty
+        };
 
     }
 }
