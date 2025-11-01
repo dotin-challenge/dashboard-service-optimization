663b76037b9ce02aea78e9cc333d192e91052a76
ehsan.soltani <ehsansoltani3398@gmail.com>
2025-11-01 12:12:34 +0330

    add cache services

---
+++ dashboard-service-optimization/CacheKeys/DashboardDataCacheKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dashboard_service_optimization.CacheKeys
+{
+    public class DashboardDataCacheKeys
+    {
+        public static string GetAllCacheKey=>"GetAllDashboardData";
+    }
+}
