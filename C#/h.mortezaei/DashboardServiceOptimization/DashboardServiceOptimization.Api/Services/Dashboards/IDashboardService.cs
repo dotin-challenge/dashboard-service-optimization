b334af065383cd7195a74b3d707dff1dbab7820d
h.mortezaei <h.mortezaei@dotin.ir>
2025-11-02 22:19:08 +0330

    بهینه‌سازی سرویس داشبورد پربازدید - h.mortezaei

---
+++ C#/h.mortezaei/DashboardServiceOptimization/DashboardServiceOptimization.Api/Services/Dashboards/IDashboardService.cs
@@ -0,0 +1,8 @@
+﻿using DashboardServiceOptimization.Api.Models;
+
+namespace DashboardServiceOptimization.Api.Services.Dashboard;
+
+public interface IDashboardService
+{
+    Task<DashboardDto> ProduceAsync(CancellationToken cancellationToken = default);
+}
