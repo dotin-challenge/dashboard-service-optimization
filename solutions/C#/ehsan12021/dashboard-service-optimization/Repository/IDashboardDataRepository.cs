8a4fea357a8c081d52ffd320ac8cc103c3f93909
d.borzouei <d.borzouei@hotmail.com>
2025-11-04 16:13:57 +0330

    chore: normalize solution path for @ehsan12021 to solutions/C#/ehsan12021
    Remove pr-* level and place files under canonical path.

---
+++ solutions/C#/ehsan12021/dashboard-service-optimization/Repository/IDashboardDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dashboard_service_optimization.Models;
+
+namespace dashboard_service_optimization.Repository
+{
+    public interface IDashboardDataRepository
+    {
+        Task<IReadOnlyCollection<DashboardDataModel>> GetDataAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ solutions/C#/ehsan12021/dashboard-service-optimization/Services/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dashboard_service_optimization.Services
+{
+    public interface ICacheService
+    {
+        Task<T> GetOrRefreshAsync<T>( string key,  Func<Task<T>> dataRetriever, TimeSpan cacheLifetime, TimeSpan softExpiration);
+    }
+}
