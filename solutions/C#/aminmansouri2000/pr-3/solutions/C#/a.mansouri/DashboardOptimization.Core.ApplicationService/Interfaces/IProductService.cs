2fde270d10d931f11833893b50a366218492892c
d.borzouei <d.borzouei@hotmail.com>
2025-11-04 16:13:55 +0330

    Revert "chore: manual merge PR #3 (non-standard)"
    This reverts commit 5cf3d4107d0b3ac58cb842ead228e428cc34c033.

--- solutions/C#/aminmansouri2000/pr-3/solutions/C#/a.mansouri/DashboardOptimization.Core.ApplicationService/Interfaces/IProductService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DashboardOptimization.Core.ApplicationService.Interfaces;
-public interface IProductService
-{
-    Task<List<ProductModel>> GetProductsAsync();
-
-    Task ClearCache();
-}
