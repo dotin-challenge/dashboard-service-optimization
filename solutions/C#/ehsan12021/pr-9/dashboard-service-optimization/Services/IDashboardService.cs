5f78766784311613e0d94a7fa37382ce11806b3d
d.borzouei <d.borzouei@hotmail.com>
2025-11-04 16:13:56 +0330

    Revert "chore: manual merge PR #9 (non-standard)"
    This reverts commit 5b32bdf582b1f2ed42554b72a13c13b09709d2be.

--- solutions/C#/ehsan12021/pr-9/dashboard-service-optimization/Services/IDashboardService.cs
+++
@@ -1,9 +0,0 @@
-﻿using dashboard_service_optimization.Models;
-
-namespace dashboard_service_optimization.Services
-{
-    public interface IDashboardService
-    {
-        Task<IEnumerable<DashboardDataModel>> GetAllDataAsync(CancellationToken cancellationToken = default);
-    }
-}
