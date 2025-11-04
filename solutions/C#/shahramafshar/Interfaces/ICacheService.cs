cc880dadb354f9c20ab1df334534586419c78c65
d.borzouei <d.borzouei@hotmail.com>
2025-11-04 15:45:36 +0330

    chore: squash-merge PR #2 from @ShahramAfshar (solutions/C#/ShahramAfshar/)
    Automated squash merge per policy. Closes #2.

---
+++ solutions/C#/shahramafshar/Interfaces/ICacheService.cs
@@ -0,0 +1,12 @@
+﻿namespace DashboardCache.Interfaces;
+
+
+public interface ICacheService
+{
+	 Task<T> GetOrCreateAsync<T>(
+		string key,
+		Func<Task<T>> factory,
+		TimeSpan absoluteExpiration,
+		TimeSpan slidingExpiration
+		);
+}
