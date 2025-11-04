301d4184261b0b52992b57163d3300b7811e111a
d.borzouei <d.borzouei@hotmail.com>
2025-11-04 15:59:03 +0330

    chore: manual merge PR #9 (non-standard)
    The PR does not follow required structure: solutions/C#/ehsan12021/
    Manually imported changes under: solutions/C#/ehsan12021/pr-9/
    Closing PR.
    Closes #9.

---
+++ solutions/C#/ehsan12021/pr-9/dashboard-service-optimization/Services/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using dashboard_service_optimization.Models;
+
+namespace dashboard_service_optimization.Services
+{
+    public interface IDashboardService
+    {
+        Task<IEnumerable<DashboardDataModel>> GetAllDataAsync(CancellationToken cancellationToken = default);
+    }
+}
